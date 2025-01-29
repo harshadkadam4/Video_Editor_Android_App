d8d79fffd564fceee2e9003ac4083e7a21b7e0de
Harshad <kadamharshad00@gmail.com>
2025-01-29 22:15:18 +0530

    Select Video

---
+++ app/src/main/java/com/example/videoeditor/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.videoeditor;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    VideoView video;
+    Button button;
+
+    ActivityResultLauncher<Intent> resultLauncher;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        video = findViewById(R.id.video);
+        button = findViewById(R.id.button);
+        
+        registerResult();
+
+        button.setOnClickListener(v -> pickVideo());
+
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(video);
+        video.setMediaController(mediaController);
+    }
+
+    private void pickVideo()
+    {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("video/*");
+        resultLauncher.launch(intent);
+    }
+
+    private void registerResult()
+    {
+        resultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getData() != null && result.getData().getData() != null){
+                            Uri videoUri = result.getData().getData();
+                            video.setVideoURI(videoUri);
+
+                            //video.start();
+                            video.setOnPreparedListener(mp -> {
+                                mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+                            });
+                            video.start();
+
+                        }
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "No Video Selected", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+}
