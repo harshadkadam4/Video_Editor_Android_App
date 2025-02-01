43e6ca6aa533be3bb30f670f07e0f017e0f6471e
Harshad <kadamharshad00@gmail.com>
2025-02-01 22:30:44 +0530

    Add RangeSlider

--- app/src/main/java/com/example/videoeditor/MainActivity.java
+++ app/src/main/java/com/example/videoeditor/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.videoeditor;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,6 +15,19 @@ import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import androidx.annotation.OptIn;
+import androidx.media3.common.C;
+import androidx.media3.common.MediaItem;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.common.util.Util;
+import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.exoplayer.source.ClippingMediaSource;
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
+import androidx.media3.exoplayer.source.MediaSource;
+import androidx.media3.transformer.DefaultAssetLoaderFactory;
+import androidx.media3.transformer.TransformationRequest;
+import androidx.media3.transformer.Transformer;
+
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -21,12 +37,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.media3.ui.PlayerView;
+
+import com.google.android.material.slider.RangeSlider;
+
+import java.io.File;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
     VideoView video;
-    Button button;
+    Button selectVideo,save;
+    RangeSlider slider;
+    ImageView trim;
+
 
     ActivityResultLauncher<Intent> resultLauncher;
 
@@ -38,17 +63,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         video = findViewById(R.id.video);
-        button = findViewById(R.id.button);
+        selectVideo = findViewById(R.id.selectVideo);
+        save  = findViewById(R.id.save);
+        slider = findViewById(R.id.slider);
+        trim  = findViewById(R.id.trim);
+
+        //slider.setValues(0f,7f);
 
         registerResult();
 
-        button.setOnClickListener(v -> pickVideo());
+        selectVideo.setOnClickListener(v -> pickVideo());
 
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(video);
         video.setMediaController(mediaController);
+
+        trim.setOnClickListener(v -> {
+
+        });
+
     }
 
+    //Videp Picking
     private void pickVideo()
     {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
@@ -65,14 +101,26 @@ public class MainActivity extends AppCompatActivity {
                     public void onActivityResult(ActivityResult result) {
                         if(result.getData() != null && result.getData().getData() != null){
                             Uri videoUri = result.getData().getData();
+
+                            // Duration Calculation
+                            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+                            retriever.setDataSource(getApplicationContext(),videoUri);
+                            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+                            long timeMillisec = Long.parseLong(time);
+                            float timeInsec = timeMillisec/1000;
+                            slider.setValueTo(timeInsec);
+                            slider.setValues(0f,timeInsec);
+
                             video.setVideoURI(videoUri);
 
                             //video.start();
                             video.setOnPreparedListener(mp -> {
                                 mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+                                save.setVisibility(View.VISIBLE);
+                                slider.setVisibility(View.VISIBLE);
+                                //slider.setValues(0f,timeInsec);
                             });
                             video.start();
-
                         }
                         else
                         {
@@ -82,4 +130,42 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
     }
+
+
+
+    //Trim Video
+    @OptIn(markerClass = UnstableApi.class)
+    public void playTrimmedVideo(Context context, String videoUri, long startMs, long endMs)
+    {
+        ExoPlayer player = new ExoPlayer.Builder(context).build();
+
+        MediaItem mediaItem = new MediaItem.Builder().setUri(Uri.parse(videoUri)).build();
+        MediaSource mediaSource = new DefaultMediaSourceFactory(context).createMediaSource(mediaItem);
+
+        ClippingMediaSource clippedSource = new ClippingMediaSource(mediaSource,
+                Util.msToUs(startMs), Util.msToUs(endMs));
+
+        player.setMediaSource(clippedSource);
+        player.prepare();
+        player.play();
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
