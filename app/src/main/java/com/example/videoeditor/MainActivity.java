bccad1e59ff87c2ba2fecbf02eff5de2a785d029
Harshad <kadamharshad00@gmail.com>
2025-02-03 21:27:39 +0530

    Implement Trim

--- app/src/main/java/com/example/videoeditor/MainActivity.java
+++ app/src/main/java/com/example/videoeditor/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.videoeditor;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.media.browse.MediaBrowser;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -12,12 +15,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import androidx.annotation.OptIn;
 import androidx.media3.common.C;
 import androidx.media3.common.MediaItem;
+import androidx.media3.common.Player;
+import androidx.media3.common.util.Log;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.common.util.Util;
 import androidx.media3.exoplayer.ExoPlayer;
@@ -25,6 +31,10 @@ import androidx.media3.exoplayer.source.ClippingMediaSource;
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory;
 import androidx.media3.exoplayer.source.MediaSource;
 import androidx.media3.transformer.DefaultAssetLoaderFactory;
+import androidx.media3.transformer.EditedMediaItem;
+import androidx.media3.transformer.EditedMediaItemSequence;
+import androidx.media3.transformer.ExportException;
+import androidx.media3.transformer.ExportResult;
 import androidx.media3.transformer.TransformationRequest;
 import androidx.media3.transformer.Transformer;
 
@@ -42,16 +52,20 @@ import androidx.media3.ui.PlayerView;
 import com.google.android.material.slider.RangeSlider;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    VideoView video;
-    Button selectVideo,save;
+    private PlayerView playerView;
+    private ExoPlayer player;
+    Button selectVideo, save;
     RangeSlider slider;
-    ImageView trim;
+    ImageView trim, crop;
+    TextView tv_trim, tv_crop;
 
+    Uri inputUri;
 
     ActivityResultLauncher<Intent> resultLauncher;
 
@@ -62,83 +76,87 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        video = findViewById(R.id.video);
+        playerView = findViewById(R.id.video);
         selectVideo = findViewById(R.id.selectVideo);
-        save  = findViewById(R.id.save);
+        save = findViewById(R.id.save);
         slider = findViewById(R.id.slider);
-        trim  = findViewById(R.id.trim);
+        trim = findViewById(R.id.trim);
+        crop = findViewById(R.id.crop);
+        tv_trim = findViewById(R.id.tv_trim);
+        tv_crop = findViewById(R.id.tv_crop);
 
-        //slider.setValues(0f,7f);
+        player = new ExoPlayer.Builder(this).build();
+        playerView.setPlayer(player);
 
         registerResult();
 
         selectVideo.setOnClickListener(v -> pickVideo());
 
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(video);
-        video.setMediaController(mediaController);
-
         trim.setOnClickListener(v -> {
+            if(inputUri != null)
+            {
+                float startMs = slider.getValues().get(0) * 1000;
+                float endMs = slider.getValues().get(1) * 1000;
 
+                trimVide(this,inputUri.toString(),(long) startMs, (long) endMs);
+            }
+            else{
+                Toast.makeText(this, "Select Video", Toast.LENGTH_SHORT).show();
+            }
         });
 
     }
 
     //Videp Picking
-    private void pickVideo()
-    {
+    private void pickVideo() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("video/*");
         resultLauncher.launch(intent);
     }
 
-    private void registerResult()
-    {
-        resultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if(result.getData() != null && result.getData().getData() != null){
-                            Uri videoUri = result.getData().getData();
+    private void registerResult() {
+        resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getData() != null && result.getData().getData() != null) {
+                    Uri videoUri = result.getData().getData();
+                    inputUri = videoUri;
 
-                            // Duration Calculation
-                            MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-                            retriever.setDataSource(getApplicationContext(),videoUri);
-                            String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-                            long timeMillisec = Long.parseLong(time);
-                            float timeInsec = timeMillisec/1000;
-                            slider.setValueTo(timeInsec);
-                            slider.setValues(0f,timeInsec);
+                    // Duration Calculation
+                    MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+                    retriever.setDataSource(getApplicationContext(), videoUri);
+                    String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+                    long timeMillisec = Long.parseLong(time);
+                    float timeInsec = timeMillisec / 1000f;
 
-                            video.setVideoURI(videoUri);
+                    //float timeInsec = video.getDuration();
+                    slider.setValueTo(timeInsec);
+                    slider.setValues(0f, timeInsec);
 
-                            //video.start();
-                            video.setOnPreparedListener(mp -> {
-                                mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
-                                save.setVisibility(View.VISIBLE);
-                                slider.setVisibility(View.VISIBLE);
-                                //slider.setValues(0f,timeInsec);
-                            });
-                            video.start();
-                        }
-                        else
-                        {
-                            Toast.makeText(MainActivity.this, "No Video Selected", Toast.LENGTH_SHORT).show();
-                        }
-                    }
+                    MediaItem mediaItem = new MediaItem.Builder().setUri(videoUri).build();
+                    player.setMediaItem(mediaItem);
+                    player.prepare();
+                    player.play();
+                    //video.start();
+                    save.setVisibility(View.VISIBLE);
+                    slider.setVisibility(View.VISIBLE);
+                    trim.setVisibility(View.VISIBLE);
+                    crop.setVisibility(View.VISIBLE);
+                    tv_trim.setVisibility(View.VISIBLE);
+                    tv_crop.setVisibility(View.VISIBLE);
+
+                } else {
+                    Toast.makeText(MainActivity.this, "Video Not Selected", Toast.LENGTH_SHORT).show();
                 }
+            }
+        }
         );
     }
 
-
-
     //Trim Video
     @OptIn(markerClass = UnstableApi.class)
-    public void playTrimmedVideo(Context context, String videoUri, long startMs, long endMs)
+    public void trimVide(Context context, String videoUri, long startMs, long endMs)
     {
-        ExoPlayer player = new ExoPlayer.Builder(context).build();
-
         MediaItem mediaItem = new MediaItem.Builder().setUri(Uri.parse(videoUri)).build();
         MediaSource mediaSource = new DefaultMediaSourceFactory(context).createMediaSource(mediaItem);
 
@@ -147,20 +165,27 @@ public class MainActivity extends AppCompatActivity {
 
         player.setMediaSource(clippedSource);
         player.prepare();
-        player.play();
+
+        player.addListener(new Player.Listener() {
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                if(playbackState == Player.STATE_READY)
+                {
+                    long durationMs = player.getDuration();
+
+                    if(durationMs != C.TIME_UNSET)
+                    {
+                        slider.setValueTo(durationMs / 1000f);
+                        slider.setValues(0f, durationMs / 1000f);
+                    }
+                    player.play();
+                }
+            }
+        });
     }
 
 
-
 }
-
-
-
-
-
-
-
-
 
 
 
